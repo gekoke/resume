3ea78a1b2020f2039104827b3827d385717d8dc3
Gregor Grigorjan <gregor@grigorjan.net>
2023-07-25 01:41:41 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      checks.${system}.spellcheck = pkgs.stdenvNoCC.mkDerivation {
+        name = "spellcheck";
+        src = ./.;
+        nativeBuildInputs = [ pkgs.nodePackages.cspell ];
+        buildPhase = ''
+          mkdir $out
+          cspell resume.html
+        '';
+      };
+
+      packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
+        name = "gregor-grigorjan-resume";
+        src = ./.;
+        __noChroot = true;
+        nativeBuildInputs = [ pkgs.chromium ];
+        buildPhase = ''
+          mkdir $out
+          chromium --headless \
+                   --disable-gpu \
+                   --no-sandbox \
+                   --run-all-compositor-stages-before-draw \
+                   --virtual-time-budget=10000 \
+                   --print-to-pdf=resume.pdf \
+                     resume.html
+        '';
+
+        installPhase = ''
+          mkdir -p $out/
+          cp resume.pdf $out/
+        '';
+      };
+    };
+}
+
